d8cdd9185151425e967c1e72e612155a0474eebf
Koutaroutarou <kw_extaro24karats@ezweb.ne.jp>
2019-06-03 03:54:43 +0900

    userdefaults課題

--- userdefaults/ViewController.swift
+++ userdefaults/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //保存されているDictionaryを読み込む箱を用意↓
+    var getDictionary: [String:String] = [:]
+    
+    
+    let saveData = UserDefaults.standard
+    
+    @IBAction func register() {
+        
+       let wordDictionary = ["vegetable": vegetable.text!, "fruits": fruit.text!]
+        
+        saveData.set(wordDictionary, forKey: "TARO")
+    }
+    
+    @IBOutlet var vegetable: UITextField!
+    
+    @IBOutlet var fruit: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+      
+        if saveData != nil {
+        //用意した箱に保存した辞書を入れる↓
+        getDictionary = saveData.dictionary(forKey: "TARO") as! [String : String]
+        //辞書から取り出してテキストフィールドに表示させる
+            vegetable.text = getDictionary["vegetable"]
+            fruit.text = getDictionary["fruits"]
+        
+        }
     }
 
 
